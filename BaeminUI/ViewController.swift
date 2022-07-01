a7c9818b55061abac06b124579bfdd334d47a65f
shddnrwls <shddnrwls@gmail.com>
2022-07-01 14:59:00 +0900

    Initial Commit

---
+++ BaeminUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaeminUI
+//
+//  Created by 노욱진 on 2022/07/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
